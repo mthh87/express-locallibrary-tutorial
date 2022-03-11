9fe17f36de7f6287c80cb19c44becddb1a295321
mthh87 <matt@harper-house.net>
2022-03-10 21:31:22 -0800

    added controllers

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://lluser:Blarg8787b00@cluster0.dvq5p.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.Connection;
+db.on('error', console.error.bind(console, 'MongoDB connection Error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
