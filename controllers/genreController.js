4ba54f5b23058e80ae9fc8d8de2fe063fcf6f8aa
mthh87 <matt@harper-house.net>
2022-03-19 14:13:34 -0700

    added genre list

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -3,7 +3,13 @@ var Genre = require('../models/genre');
 
 // Display list of all Genres.
 exports.genre_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre list');
+//    res.send('NOT IMPLEMENTED: Genre list');
+    Genre.find()
+        .sort([['name', 'ascending']])
+        .exec(function (err, list_genres) {
+        if (err) { return next (err); }
+        res.render('genre_list', { title: 'Genre List', genre_list: list_genres})
+    });
 }
 
 // Display detail page for a specific Genre
