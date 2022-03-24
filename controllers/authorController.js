bb779f98adb528014dc6adae46903c53c0f3f281
mthh87 <matt@harper-house.net>
2022-03-23 20:19:16 -0700

    added author detail page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,8 @@
 
 var Author = require('../models/author');
+var Book = require('../models/book');
+
+var async = require('async');
 
 // Display list of all Authors.
 exports.author_list = function(req, res, next) {
@@ -14,41 +17,65 @@ exports.author_list = function(req, res, next) {
 
 // Display detail page for a specific Author
 exports.author_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
-}
+    //res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+    async.parallel({
+        author: function(callback) {
+            Author.findById(req.params.id)
+                .exec(callback)
+        },
+        authors_books: function(callback) {
+            Book.find({ 'author': req.params.id }, 'title summary')
+                .exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); } // error in API usage
+        if (results.author==null) { // no results
+            var err = new Error('Author not found');
+            err.status = 404;
+            return next(err);
+        }
+        // found something
+        res.render('author_detail', { 
+            title: 'Author Detail',
+            author: results.author,
+            author_books: results.authors_books
+        });
+        //console.log({title: 'Author Detail', author: results.author, author_books: results.authors_books});
+    });
+};
 
 
 // Display Author create form on GET
 exports.author_create_get = function(req, res) {
     res.send('NOT IMPLEMENTED: Author create GET');
-}
+};
 
 
 // Handle Author create on POST
 exports.author_create_post = function(req, res) {
     res.send('NOT IMPLEMENTED: Author create POST');
-}
+};
 
 
 // Display Author delete form on GET
 exports.author_delete_get = function(req, res) {
     res.send('NOT IMPLEMENTED: Author delete GET');
-}
+};
 
 
 // Handle Author delete on POST
 exports.author_delete_post = function(req, res) {
     res.send('NOT IMPLEMENTED: Author delete POST');
-}
+};
 
 
 // Display Author update form on GET
 exports.author_update_get = function(req, res) {
     res.send('NOT IMPLEMENTED: Author update GET');
-}
+};
 
 
 // Handle Author update on POST
 exports.author_update_post = function(req, res) {
     res.send('NOT IMPLEMENTED: Author update POST');
-}
+};
